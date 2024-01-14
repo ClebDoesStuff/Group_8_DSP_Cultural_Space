8e83635fb1d0bb28071ec0ffe9ad0006b07597ec
Macash <44979269+MacashD@users.noreply.github.com>
2024-01-14 21:33:07 +0000

    minigame changes commit to main

--- kingston/Assets/script/teleport.cs
+++ kingston/Assets/script/teleport.cs
@@ -5,16 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class teleport : MonoBehaviour
 {
-    Rigidbody rb;
-    // Start is called before the first frame update
+    public string teleporterNo;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (teleporterNo == "1"){ Playeritems.teleporter1 = false; }
+            if (teleporterNo == "2"){ Playeritems.teleporter2 = false; }
+            if (teleporterNo == "3"){ Playeritems.teleporter3 = false; }
+            if (teleporterNo == "4"){ Playeritems.teleporter4 = false; }
+            SceneManager.LoadScene(1);
+        }
+    }
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void OnCollisionEnter(Collision collision)
-    {
-        SceneManager.LoadScene(1);
+        if (teleporterNo == "1" && Playeritems.teleporter1 == false) { transform.gameObject.SetActive(false); }    
+        if (teleporterNo == "2" && Playeritems.teleporter2 == false) { transform.gameObject.SetActive(false); }    
+        if (teleporterNo == "3" && Playeritems.teleporter3 == false) { transform.gameObject.SetActive(false); }    
+        if (teleporterNo == "4" && Playeritems.teleporter4 == false) { transform.gameObject.SetActive(false); }
     }
 }
